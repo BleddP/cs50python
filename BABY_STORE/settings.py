7ae3f5f009c4b8d70d4be8040f4a076be0c62f93
BleddP <pijpersbleddyn@gmail.com>
2021-03-10 21:30:02 +0800

    first commit

--- BABY_STORE/settings.py
+++ BABY_STORE/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from . import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o#q7z6ke$ha+0n85fr)hix66ma6mqef5ed@+_67f@vfs4nsd6f'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
